93bf8cba011280591717ed67a125ac926d73c461
Amer Fatic <58990748+faticamer@users.noreply.github.com>
2023-05-16 00:50:58 +0200

    Create bag.cpp

---
+++ cpp-files/bag.cpp
@@ -0,0 +1,4 @@
+#include "bag.h"
+
+std::ofstream sales("canteen_reports/sales.txt", std::ios_base::app); // define and initialize the object
+std::ofstream inventory("canteen_reports/inventory.txt");
